36604415b94ccc4595a8df25bb56abee7e1b2338
José Olórtegui <20072509+olrtg@users.noreply.github.com>
2022-02-11 16:55:44 -0300

    added prettier and eslint in null-ls

--- nvim/lua/user/lsp/null-ls.lua
+++ nvim/lua/user/lsp/null-ls.lua
@@ -11,9 +11,9 @@ local diagnostics = null_ls.builtins.diagnostics
 null_ls.setup({
 	debug = false,
 	sources = {
-		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
-		formatting.black.with({ extra_args = { "--fast" } }),
+		formatting.prettier,
 		formatting.stylua,
-    -- diagnostics.flake8
+		-- formatting.black.with({ extra_args = { "--fast" } }),
+    diagnostics.eslint
 	},
 })
