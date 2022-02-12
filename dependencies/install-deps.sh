3b5f35cf15d18577887a374e49558f7eb3006a78
José Olórtegui <20072509+olrtg@users.noreply.github.com>
2022-02-12 19:55:29 -0300

    using eslint instead of eslint_d

--- dependencies/install-deps.sh
+++ dependencies/install-deps.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Node
-npm i -g pnpm @antfu/ni nvim prettier eslint_d jsonlint @angular/cli
+npm i -g pnpm @antfu/ni nvim prettier eslint jsonlint @angular/cli
 
 # Python
 pip3 pynvim
