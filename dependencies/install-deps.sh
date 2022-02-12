8ed1ba7f568b641636ad9be255389b73bcb25472
José Olórtegui <20072509+olrtg@users.noreply.github.com>
2022-02-12 19:22:16 -0300

    moved the installation of dependencies in one file

---
+++ dependencies/install-deps.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Node
+npm i -g pnpm @antfu/ni nvim prettier eslint_d jsonlint @angular/cli
+
+# Python
+pip3 pynvim
+
+# Cargo
+cargo install stylua
+
+# Luarocks
+luarocks install luacheck
