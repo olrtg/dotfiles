648c097384e3fc01b87539533ec2172122f5e249
José Olórtegui <20072509+olrtg@users.noreply.github.com>
2022-02-12 12:37:05 -0300

    migrated to nvchad

--- nvim/lua/user/colorscheme.lua
+++
@@ -1,9 +0,0 @@
-vim.cmd [[
-try
-  let g:gruvbox_material_transparent_background = 1
-  colorscheme gruvbox-material
-catch /^Vim\%((\a\+)\)\=:E185/
-  colorscheme default
-  set background=dark
-endtry
-]]
