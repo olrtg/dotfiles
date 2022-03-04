e4c2e887ebc437d35a2f219db15bfcfa175c3f3d
José Olórtegui <20072509+olrtg@users.noreply.github.com>
2022-03-04 15:38:04 -0300

    fix: neovim package, chore: removed unused package

--- dependencies/install-deps.sh
+++ dependencies/install-deps.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 
 # Node
-npm i -g pnpm @antfu/ni nvim prettier eslint jsonlint @angular/cli fast-cli npm-check
+npm i -g pnpm @antfu/ni neovim prettier eslint jsonlint @angular/cli fast-cli npm-check
 
 # Python
 pip3 pynvim
 
 # Cargo
 cargo install stylua
-
-# Luarocks
-luarocks install luacheck
