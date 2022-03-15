ee90a936e4552be3f36d3ff737dd6d9a9ebde3e4
José Olórtegui <20072509+olrtg@users.noreply.github.com>
2022-03-15 18:50:32 -0300

    feat: custom theme for bufferline

---
+++ lvim/lua/plugins/bufferline.lua
@@ -0,0 +1,53 @@
+local Bufferline = {}
+
+Bufferline.config = function()
+	if not lvim.builtin.bufferline.active then
+		return
+	end
+
+	lvim.builtin.bufferline.options = {
+		indicator_icon = " ",
+		offsets = {
+			{ filetype = "NvimTree" },
+		},
+	}
+
+	lvim.builtin.bufferline.highlights = {
+		background = {
+			guibg = { attribute = "bg", highlight = "EndOfBuffer" },
+			guifg = { attribute = "fg", highlight = "LineNr" },
+		},
+		fill = {
+			guibg = { attribute = "bg", highlight = "EndOfBuffer" },
+			guifg = { attribute = "fg", highlight = "EndOfBuffer" },
+		},
+		separator = {
+			guifg = { attribute = "fg", highlight = "EndOfBuffer" },
+			guibg = { attribute = "bg", highlight = "EndOfBuffer" },
+		},
+		close_button = {
+			guibg = { attribute = "bg", highlight = "EndOfBuffer" },
+			guifg = { attribute = "fg", highlight = "LineNr" },
+		},
+		close_button_visible = {
+			guibg = { attribute = "bg", highlight = "EndOfBuffer" },
+			guifg = { attribute = "fg", highlight = "LineNr" },
+		},
+		close_button_selected = {
+			guibg = { attribute = "bg", highlight = "EndOfBuffer" },
+			guifg = { attribute = "fg", highlight = "LineNr" },
+		},
+		buffer_visible = {
+			guibg = { attribute = "bg", highlight = "EndOfBuffer" },
+			guifg = { attribute = "fg", highlight = "LineNr" },
+		},
+		modified = {
+			guibg = { attribute = "bg", highlight = "EndOfBuffer" },
+		},
+		modified_visible = {
+			guibg = { attribute = "bg", highlight = "EndOfBuffer" },
+		},
+	}
+end
+
+return Bufferline
