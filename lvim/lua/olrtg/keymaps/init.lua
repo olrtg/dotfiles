188978f61120ecca1f8d06cc6098666764d9c6f0
José Olórtegui <20072509+olrtg@users.noreply.github.com>
2022-05-12 07:30:18 -0400

    feat: added trouble and persistance plugins

--- lvim/lua/olrtg/keymaps/init.lua
+++ lvim/lua/olrtg/keymaps/init.lua
@@ -1,4 +1,3 @@
--- Which-key: +Harpoon
 lvim.builtin.which_key.mappings["h"] = {
 	name = "+Harpoon",
 	a = { "<cmd>lua require('harpoon.ui').nav_file(1)<cr>", "Navigate to file 1" },
@@ -9,9 +8,25 @@ lvim.builtin.which_key.mappings["h"] = {
 	u = { "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", "Toggle UI" },
 }
 
--- Which-key: +Git
 lvim.builtin.which_key.mappings["W"] = {
 	name = "+Worktree",
 	s = { "<cmd>lua require('telescope').extensions.git_worktree.git_worktrees()<cr>", "Switch/Delete" },
 	c = { "<cmd>lua require('telescope').extensions.git_worktree.create_git_worktree()<cr>", "Create" },
+}
+
+lvim.builtin.which_key.mappings["t"] = {
+	name = "Diagnostics",
+	t = { "<cmd>TroubleToggle<cr>", "trouble" },
+	w = { "<cmd>TroubleToggle lsp_workspace_diagnostics<cr>", "workspace" },
+	d = { "<cmd>TroubleToggle lsp_document_diagnostics<cr>", "document" },
+	q = { "<cmd>TroubleToggle quickfix<cr>", "quickfix" },
+	l = { "<cmd>TroubleToggle loclist<cr>", "loclist" },
+	r = { "<cmd>TroubleToggle lsp_references<cr>", "references" },
+}
+
+lvim.builtin.which_key.mappings["S"] = {
+	name = "Session",
+	c = { "<cmd>lua require('persistence').load()<cr>", "Restore last session for current dir" },
+	l = { "<cmd>lua require('persistence').load({ last = true })<cr>", "Restore last session" },
+	Q = { "<cmd>lua require('persistence').stop()<cr>", "Quit without saving session" },
 }
--- lvim/lua/olrtg/plugins/init.lua
+++ lvim/lua/olrtg/plugins/init.lua
@@ -81,6 +81,23 @@ lvim.plugins = {
 			})
 		end,
 	},
+
+	{
+		"folke/trouble.nvim",
+		cmd = "TroubleToggle",
+	},
+
+	{
+		"folke/persistence.nvim",
+		event = "BufReadPre", -- this will only start session saving when an actual file was opened
+		module = "persistence",
+		config = function()
+			require("persistence").setup({
+				dir = vim.fn.expand(vim.fn.stdpath("config") .. "/session/"),
+				options = { "buffers", "curdir", "tabpages", "winsize" },
+			})
+		end,
+	},
 }
 
 require("telescope").load_extension("git_worktree")
