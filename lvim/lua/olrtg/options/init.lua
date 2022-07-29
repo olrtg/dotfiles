a59b1dea1b6db186dcb868e8bde60d1612db2b19
José Olórtegui <20072509+olrtg@users.noreply.github.com>
2022-07-28 22:58:52 -0400

    2s timout format, tree config

--- lvim/lua/olrtg/options/init.lua
+++ lvim/lua/olrtg/options/init.lua
@@ -1,7 +1,7 @@
--- General
 lvim.log.level = "warn"
 
 lvim.format_on_save = true
+-- lvim.format_on_save.timeout = 2000
 
 vim.o.background = "dark"
 vim.g.everforest_background = "dark"
@@ -20,12 +20,12 @@ lvim.leader = "space"
 -- Builtins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.alpha.dashboard.section.header.val = {
-  [[                               __                ]],
-  [[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
-  [[ / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
-  [[/\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
-  [[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
-  [[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
+	[[                               __                ]],
+	[[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
+	[[ / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
+	[[/\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
+	[[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
+	[[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
 }
 
 lvim.builtin.notify.active = true
@@ -34,8 +34,11 @@ lvim.builtin.terminal.active = true
 
 lvim.builtin.dap.active = true
 
-lvim.builtin.nvimtree.setup.view.side = "left"
+lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
+lvim.builtin.nvimtree.setup.view.side = "right"
 lvim.builtin.nvimtree.setup.view.width = 40
 
 lvim.builtin.treesitter.highlight.enabled = true
 lvim.builtin.treesitter.rainbow.enable = true
+
+lvim.builtin.lualine.options.globalstatus = true
