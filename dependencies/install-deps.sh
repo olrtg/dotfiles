0d4159fdd2c76fb483c9c6f88442278d093c6c39
José Olórtegui <20072509+olrtg@users.noreply.github.com>
2022-10-31 20:18:23 -0300

    chore: removed deps now managed by mason, added commitizen

--- dependencies/install-deps.sh
+++ dependencies/install-deps.sh
@@ -1,24 +1,16 @@
 #!/bin/sh
 
 # Node
-npm i -g pnpm @antfu/ni \
+npm i -g pnpm \
+	@antfu/ni \
 	neovim \
 	typescript \
-	@fsouza/prettierd \
-	eslint_d \
-	jsonlint \
-	markdownlint-cli \
-	write-good \
-	stylelint \
-	rustywind \
 	@angular/cli \
 	react-native \
-	fast-cli \
 	npm-check \
-	yarn
+	yarn \
+	commitizen \
+	cz-conventional-changelog
 
 # Python
 pip3 pynvim
-
-# Cargo
-cargo install stylua
