f53b3f1f737123e3471259df842577c61f9a1863
José Olórtegui <20072509+olrtg@users.noreply.github.com>
2023-02-03 12:29:11 -0300

    chore(lvim): disable dap configuration for flutter

--- lvim/lua/user/plugins.lua
+++ lvim/lua/user/plugins.lua
@@ -58,39 +58,39 @@ M.flutter_tools_setup = function()
       on_attach = require("lvim.lsp").common_on_attach,
       color = { enabled = true },
     },
-    debugger = {
-      enabled = true,
-      run_via_dap = true,
-      -- don't pause on exception in flutter
-      exception_breakpoints = {},
-      register_configurations = function(paths)
-        require("dap").configurations.dart = {
-          {
-            name = "Launch App",
-            request = "launch",
-            type = "dart",
-            dartSdkPath = paths.dart_sdk,
-            flutterSdkPath = paths.flutter_sdk,
-          },
-          {
-            name = "Launch App (profile mode)",
-            request = "launch",
-            type = "dart",
-            flutterMode = "profile",
-            dartSdkPath = paths.dart_sdk,
-            flutterSdkPath = paths.flutter_sdk,
-          },
-          {
-            name = "Launch App (release mode)",
-            request = "launch",
-            type = "dart",
-            flutterMode = "release",
-            dartSdkPath = paths.dart_sdk,
-            flutterSdkPath = paths.flutter_sdk,
-          },
-        }
-      end,
-    },
+    -- debugger = {
+    --   enabled = true,
+    --   run_via_dap = true,
+    --   -- don't pause on exception in flutter
+    --   exception_breakpoints = {},
+    --   register_configurations = function(paths)
+    --     require("dap").configurations.dart = {
+    --       {
+    --         name = "Launch App",
+    --         request = "launch",
+    --         type = "dart",
+    --         dartSdkPath = paths.dart_sdk,
+    --         flutterSdkPath = paths.flutter_sdk,
+    --       },
+    --       {
+    --         name = "Launch App (profile mode)",
+    --         request = "launch",
+    --         type = "dart",
+    --         flutterMode = "profile",
+    --         dartSdkPath = paths.dart_sdk,
+    --         flutterSdkPath = paths.flutter_sdk,
+    --       },
+    --       {
+    --         name = "Launch App (release mode)",
+    --         request = "launch",
+    --         type = "dart",
+    --         flutterMode = "release",
+    --         dartSdkPath = paths.dart_sdk,
+    --         flutterSdkPath = paths.flutter_sdk,
+    --       },
+    --     }
+    --   end,
+    -- },
     widget_guides = { enabled = true },
   })
 end
