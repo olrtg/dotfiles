7a1a3594eeed7f2b5aff59dce86d461043dd04e3
José Olórtegui <20072509+olrtg@users.noreply.github.com>
2023-02-09 12:24:52 -0300

    feat(nvim): basic config for lazyvim

---
+++ nvim/lua/plugins/editor.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      window = { position = "right" },
+      filesystem = { filtered_items = { hide_dotfiles = false, hide_gitignored = false } },
+    },
+  },
+}
