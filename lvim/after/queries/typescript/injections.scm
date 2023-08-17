e8983d9ff0a4ea94844337813c83ceafb32968b1
José Olórtegui <20072509+olrtg@users.noreply.github.com>
2023-08-17 16:15:41 -0400

    feat(lvim): graphql injection support for string literals

--- lvim/after/queries/typescript/injections.scm
+++ lvim/after/queries/typescript/injections.scm
@@ -1,5 +1,5 @@
 ;; extends
 (
-  (string_fragment) @injection.content (#match? @injection.content "^query|mutation")
+  [(string_fragment) (template_string)] @injection.content (#match? @injection.content "^query|mutation|`query|`mutation")
   (#set! injection.language "graphql")
 )
