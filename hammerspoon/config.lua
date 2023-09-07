950b5a5a06e521c0df929f966a83f921fcf8f354
José Olórtegui <20072509+olrtg@users.noreply.github.com>
2023-09-06 20:09:27 -0400

    chore: drop hammerspoon config

--- hammerspoon/config.lua
+++
@@ -1,7 +0,0 @@
-local hyper = { "ctrl", "alt", "shift" } -- Bound to CapsLock in Karabiner-Elements
-local hyperCmd = { "ctrl", "alt", "shift", "cmd" } -- CapsLock + cmd
-
-return {
-	hyper = hyper,
-	hyperCmd = hyperCmd,
-}
