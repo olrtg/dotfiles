a33bfaefce984b860a1adf8fcf30d7d8568e540b
José Olórtegui <20072509+olrtg@users.noreply.github.com>
2023-09-23 12:02:29 -0400

    feat(lvim): add harpoon

--- lvim/config.lua
+++ lvim/config.lua
@@ -102,6 +102,23 @@ lvim.plugins = {
     end,
   },
 
+  {
+    "ThePrimeagen/harpoon",
+    dependencies = "nvim-lua/plenary.nvim",
+    config = function()
+      require("harpoon").setup()
+      lvim.builtin.which_key.mappings["m"] = {
+        name = "Harpoon",
+        m = { require("harpoon.mark").add_file, "Mark File" },
+        a = { '<cmd>lua require("harpoon.ui").nav_file(1)<cr>', "Go to Mark 1" },
+        s = { '<cmd>lua require("harpoon.ui").nav_file(2)<cr>', "Go to Mark 2" },
+        d = { '<cmd>lua require("harpoon.ui").nav_file(3)<cr>', "Go to Mark 3" },
+        f = { '<cmd>lua require("harpoon.ui").nav_file(4)<cr>', "Go to Mark 4" },
+      }
+      lvim.builtin.which_key.mappings["<leader>"] = { require("harpoon.ui").toggle_quick_menu, "Marks Menu" }
+    end,
+  },
+
   -- NOTE: My plugins in development
   {
     dir = "~/i/nvim-i18n",
