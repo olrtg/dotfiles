0d73357379fce931ed19e97665d4076e5c698d47
José Olórtegui <20072509+olrtg@users.noreply.github.com>
2023-10-03 20:07:40 -0300

    feat(lvim): java setup

--- lvim/lua/user/languages/java.lua
+++ lvim/lua/user/languages/java.lua
@@ -1,5 +1,11 @@
 local api = require("user.utils.api")
 
-api.install_tools({ "jdtls" })
+api.override_servers({ "jdtls" })
+
+api.install_tools({ "google-java-format" })
+
+api.install_plugins({
+  "mfussenegger/nvim-jdtls",
+})
 
 api.install_parsers({ "java" })
