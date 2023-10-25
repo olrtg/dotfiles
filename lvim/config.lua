6371fd03e56b4cf9252f3b43d079242aecb0d6b9
José Olórtegui <20072509+olrtg@users.noreply.github.com>
2023-10-25 08:15:07 -0300

    chore: latest

--- lvim/config.lua
+++ lvim/config.lua
@@ -5,6 +5,7 @@ lvim.format_on_save.enabled = true
 lvim.format_on_save.timeout = 5000
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
 lvim.builtin.alpha.active = false
+lvim.builtin.bufferline.active = false
 
 -- https://github.com/nvim-tree/nvim-tree.lua/wiki/Recipes#center-a-floating-nvim-tree-window
 local HEIGHT_RATIO = 0.8
