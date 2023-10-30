0d59dfb0123bee7eef2e65ee96e2461955a7456c
José Olórtegui <20072509+olrtg@users.noreply.github.com>
2023-10-30 18:55:42 -0300

    chore(macos): larger font for macos

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -6,9 +6,14 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
+if wezterm.target_triple == "aarch64-apple-darwin" or wezterm.target_triple == "x86_64-apple-darwin" then
+	config.font_size = 14.0
+else
+	config.font_size = 12.0
+end
+
 config.color_scheme = "Tokyo Night"
 config.font = wezterm.font("Comic Code")
-config.font_size = 13.0
 config.line_height = 1.2
 config.warn_about_missing_glyphs = false
 config.hide_tab_bar_if_only_one_tab = true
