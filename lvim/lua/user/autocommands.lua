079294af43d6e46c69936c6c39e575cc7ac5ad9b
José Olórtegui <20072509+olrtg@users.noreply.github.com>
2023-11-02 08:44:25 -0300

    feat(lvim): open help in verticla split

--- lvim/lua/user/autocommands.lua
+++ lvim/lua/user/autocommands.lua
@@ -9,3 +9,14 @@ vim.api.nvim_create_autocmd(
   { "BufNewFile", "BufRead" },
   { pattern = "Brewfile,.Brewfile", command = "set filetype=ruby" }
 )
+
+-- Open help in vertical split
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  group = vim.api.nvim_create_augroup("help_window_right", {}),
+  pattern = { "*.txt" },
+  callback = function()
+    if vim.o.filetype == "help" then
+      vim.cmd.wincmd("L")
+    end
+  end,
+})
