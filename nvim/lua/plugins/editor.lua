ba88c4d73953741d02aec5b9869e3cb61124a5b5
José Olórtegui <20072509+olrtg@users.noreply.github.com>
2024-02-02 12:14:16 -0300

    Revert "chore(nvim): add harpoon"
    This reverts commit 64c14c8308c339792f2842ae218315069c3b0718.

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -7,21 +7,4 @@ return {
   },
 
   { "dmmulroy/tsc.nvim", config = true },
-
-  {
-    "ThePrimeagen/harpoon",
-    dependencies = "nvim-lua/plenary.nvim",
-    config = function()
-      require("harpoon").setup()
-      lvim.builtin.which_key.mappings["m"] = {
-        name = "Harpoon",
-        m = { require("harpoon.mark").add_file, "Mark File" },
-        a = { '<cmd>lua require("harpoon.ui").nav_file(1)<cr>', "Go to Mark 1" },
-        s = { '<cmd>lua require("harpoon.ui").nav_file(2)<cr>', "Go to Mark 2" },
-        d = { '<cmd>lua require("harpoon.ui").nav_file(3)<cr>', "Go to Mark 3" },
-        f = { '<cmd>lua require("harpoon.ui").nav_file(4)<cr>', "Go to Mark 4" },
-      }
-      lvim.builtin.which_key.mappings["<leader>"] = { require("harpoon.ui").toggle_quick_menu, "Marks Menu" }
-    end,
-  },
 }
