07c6fa641e004878f43f02337481f4457c6ec32b
José Olórtegui <20072509+olrtg@users.noreply.github.com>
2024-04-15 04:00:16 -0400

    feat(nvim-vscode): stylua as default formatter, highlight on yank, lazygit

--- nvim-vscode/init.lua
+++ nvim-vscode/init.lua
@@ -1,6 +1,14 @@
 vim.g.mapleader = " "
 vim.opt.clipboard = "unnamedplus"
 
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank({ timeout = 300 })
+	end,
+})
+
 -- [[ Lazy ]]
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -37,6 +45,12 @@ end)
 vim.keymap.set("n", "<leader>st", function()
 	vscode.call("workbench.action.findInFiles")
 end)
+vim.keymap.set("n", "<leader>e", function()
+	vscode.call("workbench.view.explorer")
+end)
+vim.keymap.set("n", "<leader>gg", function()
+	vscode.call("lazygit.openLazygit")
+end)
 
 -- [[ LSP ]]
 vim.keymap.set("n", "gs", function()
@@ -56,7 +70,6 @@ end)
 vim.keymap.set("n", "<S-h>", function()
 	vscode.call("workbench.action.previousEditor")
 end)
-
 vim.keymap.set("n", "<S-l>", function()
 	vscode.call("workbench.action.nextEditor")
 end)
