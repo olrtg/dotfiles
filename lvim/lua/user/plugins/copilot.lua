71c93d871ace4aaf13ae6841830ffce6a434fa73
José Olórtegui <20072509+olrtg@users.noreply.github.com>
2024-05-01 01:53:09 -0400

    feat(lvim): improve copilot config

--- lvim/lua/user/plugins/copilot.lua
+++ lvim/lua/user/plugins/copilot.lua
@@ -2,10 +2,11 @@ local api = require("user.utils.api")
 
 api.install_plugins({
   {
+    "zbirenbaum/copilot.lua",
     cmd = "Copilot",
     event = "InsertEnter",
-    "zbirenbaum/copilot.lua",
     opts = {
+      panel = { enabled = false },
       suggestion = {
         auto_trigger = true,
         keymap = {
@@ -16,6 +17,15 @@ api.install_plugins({
           prev = "<M-[>",
           dismiss = "<M-c>",
         },
+      },
+      filetypes = {
+        sh = function()
+          -- NOTE: disable for .env files
+          if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), "^%.env.*") then
+            return false
+          end
+          return true
+        end,
       },
     },
   },
