0cbbceed0c0838db52795316712a59d949396a8b
José Olórtegui <20072509+olrtg@users.noreply.github.com>
2024-05-31 19:11:05 -0400

    feat(nvim): all filetypes supported by prettier

--- nvim/lua/custom/formatter.lua
+++ nvim/lua/custom/formatter.lua
@@ -1,10 +1,23 @@
 require("conform").setup({
 	formatters_by_ft = {
-		lua = { "stylua" },
-		javascript = { "prettierd" },
-		typescript = { "prettierd" },
-		javascriptreact = { "prettierd" },
-		typescriptreact = { "prettierd" },
+		["lua"] = { "stylua" },
+
+		["javascript"] = { "prettierd" },
+		["typescript"] = { "prettierd" },
+		["javascriptreact"] = { "prettierd" },
+		["typescriptreact"] = { "prettierd" },
+		["vue"] = { "prettierd" },
+		["css"] = { "prettierd" },
+		["scss"] = { "prettierd" },
+		["less"] = { "prettierd" },
+		["html"] = { "prettierd" },
+		["json"] = { "prettierd" },
+		["jsonc"] = { "prettierd" },
+		["yaml"] = { "prettierd" },
+		["markdown"] = { "prettierd" },
+		["markdown.mdx"] = { "prettierd" },
+		["graphql"] = { "prettierd" },
+		["handlebars"] = { "prettierd" },
 	},
 })
 
