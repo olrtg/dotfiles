741fb6d400b5e91eb263af3639493e543074431a
José Olórtegui <20072509+olrtg@users.noreply.github.com>
2024-06-06 10:26:08 -0400

    chore(terminal): change font size

--- single/.wezterm.lua
+++ single/.wezterm.lua
@@ -7,13 +7,13 @@ if wezterm.config_builder then
 end
 
 if wezterm.target_triple == "aarch64-apple-darwin" or wezterm.target_triple == "x86_64-apple-darwin" then
-	config.font_size = 16.0
-else
 	config.font_size = 14.0
+else
+	config.font_size = 12.0
 end
 
 config.color_scheme = "OneDark (base16)"
-config.font = wezterm.font("Berkeley Mono")
+config.font = wezterm.font("JetBrainsMono Nerd Font")
 config.line_height = 1.2
 config.warn_about_missing_glyphs = false
 config.hide_tab_bar_if_only_one_tab = true
